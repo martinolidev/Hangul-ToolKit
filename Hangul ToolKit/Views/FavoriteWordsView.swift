74a65adc0ff682ce2de0e4a71589267550684e5e
Martin Martinez <mtz.martin@proton.me>
2024-03-25 21:56:07 -0600

    Navigation title added

--- Hangul ToolKit/Views/FavoriteWordsView.swift
+++ Hangul ToolKit/Views/FavoriteWordsView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct FavoriteWordsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("List here")
+        }.navigationTitle("Favorite Words")
     }
 }
 
 #Preview {
-    FavoriteWordsView()
+    ContentView()
 }
