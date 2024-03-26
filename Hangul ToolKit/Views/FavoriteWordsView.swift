db1f119ffbbac5aee0f01a1db1e384d68b8cf2bf
Martin Martinez <mtz.martin@proton.me>
2024-03-25 21:54:24 -0600

    Flie where I will show the favorite words of the user, they will persist

---
+++ Hangul ToolKit/Views/FavoriteWordsView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteWordsView.swift
+//  Hangul ToolKit
+//
+//  Created by Martin Martinez on 25/03/24.
+//
+
+import SwiftUI
+
+struct FavoriteWordsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteWordsView()
+}
