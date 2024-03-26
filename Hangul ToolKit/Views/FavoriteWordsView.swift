2c39bc27ac6bf6c4ee0281c981d29f8c4135aadb
Martin Martinez <mtz.martin@proton.me>
2024-03-25 22:16:10 -0600

    Now it uses each row for every item, good, I will add a delete method tomorrow and take the date without time

--- Hangul ToolKit/Views/FavoriteWordsView.swift
+++ Hangul ToolKit/Views/FavoriteWordsView.swift
@@ -6,11 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct FavoriteWordsView: View {
+    @Environment(\.modelContext) private var context
+    @Query private var words: [FavoriteWords]
+    
     var body: some View {
         VStack {
-            Text("List here")
+            List {
+                ForEach(words) { word in
+                    HStack {
+                        Text(word.koreanWord)
+                        Text(word.latinWord)
+                        Text("\(word.date)")
+                    }
+                }
+            }
         }.navigationTitle("Favorite Words")
     }
 }
