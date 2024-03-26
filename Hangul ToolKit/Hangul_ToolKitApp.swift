2635737151eed0c5b7badd3d660112c1d103d930
Martin Martinez <mtz.martin@proton.me>
2024-03-25 19:55:14 -0600

    Initial Commit

---
+++ Hangul ToolKit/Hangul_ToolKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hangul_ToolKitApp.swift
+//  Hangul ToolKit
+//
+//  Created by Martin Martinez on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Hangul_ToolKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
