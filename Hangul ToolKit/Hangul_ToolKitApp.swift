e690c5c595791c06a525b2965609b5f77491c9ca
Martin Martinez <mtz.martin@proton.me>
2024-03-25 21:59:13 -0600

    Container for my data

--- Hangul ToolKit/Hangul_ToolKitApp.swift
+++ Hangul ToolKit/Hangul_ToolKitApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct Hangul_ToolKitApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+        }.modelContainer(for: FavoriteWords.self)
     }
 }
