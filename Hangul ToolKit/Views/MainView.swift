a2f00ce56066e29259ec436b70d84d82a62b9336
Martin Martinez <mtz.martin@proton.me>
2024-04-02 14:09:50 -0600

    Scroll size fixed

--- Hangul ToolKit/Views/MainView.swift
+++ Hangul ToolKit/Views/MainView.swift
@@ -22,86 +22,88 @@ struct MainView: View {
     @Environment(\.modelContext) private var context
     
     var body: some View {
-        VStack {
-            Image("Transformer")
-                .resizable()
-                .frame(width: 180, height: 180)
-                .padding()
-            
-            HStack {
-                Text("Hangul to Latin")
-                    .font(.title2)
-                    .bold()
+        ScrollView {
+            VStack {
+                Image("Transformer")
+                    .resizable()
+                    .frame(width: 180, height: 180)
+                    .padding()
+                
+                HStack {
+                    Text("Hangul to Latin")
+                        .font(.title2)
+                        .bold()
+                    Spacer()
+                    Button("Add to your favorites") {
+                        if input.isEmpty {
+                            showAlert = true
+                        } else {
+                            addToFavorite(korean: input, latin: latin)
+                        }
+                    }.foregroundStyle(.indigo)
+                }.padding()
+                
+                HStack {
+                    TextField("Type a korean word", text: $input)
+                        .padding(.top)
+                        .frame(height: 30)
+                    
+                    Button(action: {
+                        if input.isEmpty {
+                            showAlert = true
+                        } else {
+                            latin = input.applyingTransform(.toLatin, reverse: false) ?? ""
+                            textToSpeech(text: input)
+                        }
+                    }) {
+                        Image(systemName: "waveform.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundStyle(.indigo)
+                    }
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text("Empty field"), message: Text("Type a korean word"), dismissButton: .default(Text("Ok")))
+                    }
+                }.padding(.horizontal)
+                
+                HStack(spacing: 100) {
+                    VStack(spacing: 20) {
+                        Text("Hangul")
+                            .font(.title3)
+                            .bold()
+                        Text(input)
+                            .bold()
+                    }
+                    
+                    VStack(spacing: 20) {
+                        Text("Pronunciation")
+                            .font(.title3)
+                            .bold()
+                        Text(latin)
+                            .bold()
+                    }
+                }
+                .padding(.top)
+                
+                
                 Spacer()
-                Button("Add to your favorites") {
-                    if input.isEmpty {
-                        showAlert = true
-                    } else {
-                        addToFavorite(korean: input, latin: latin)
+                Section {
+                    
+                } footer: {
+                    Text("Make sure you installed the Korean keyboard")
+                        .foregroundStyle(.gray)
+                        .font(.caption)
+                }
+            }.toolbar {
+                ToolbarItem {
+                    NavigationLink(destination: SettingsView(speech: speechSettings)) {
+                        Image(systemName: "gearshape.fill")
+                            .foregroundStyle(.indigo)
                     }
-                }.foregroundStyle(.indigo)
-            }.padding()
-            
-            HStack {
-                TextField("Type a korean word", text: $input)
-                    .padding(.top)
-                    .frame(height: 30)
-                
-                Button(action: {
-                    if input.isEmpty {
-                        showAlert = true
-                    } else {
-                        latin = input.applyingTransform(.toLatin, reverse: false) ?? ""
-                        textToSpeech(text: input)
-                    }
-                }) {
-                    Image(systemName: "waveform.circle.fill")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .foregroundStyle(.indigo)
-                }
-                .alert(isPresented: $showAlert) {
-                    Alert(title: Text("Empty field"), message: Text("Type a korean word"), dismissButton: .default(Text("Ok")))
-                }
-            }.padding(.horizontal)
-            
-            HStack(spacing: 100) {
-                VStack(spacing: 20) {
-                    Text("Hangul")
-                        .font(.title3)
-                        .bold()
-                    Text(input)
-                        .bold()
-                }
-                
-                VStack(spacing: 20) {
-                    Text("Pronunciation")
-                        .font(.title3)
-                        .bold()
-                    Text(latin)
-                        .bold()
-                }
-            }
-            .padding(.top)
-            
-            
-            Spacer()
-            Section {
-                
-            } footer: {
-                Text("Make sure you installed the Korean keyboard")
-                    .foregroundStyle(.gray)
-                    .font(.caption)
-            }
-        }.toolbar {
-            ToolbarItem {
-                NavigationLink(destination: SettingsView(speech: speechSettings)) {
-                    Image(systemName: "gearshape.fill")
-                        .foregroundStyle(.indigo)
                 }
             }
         }
-    }
+        }
     
     func textToSpeech(text: String) {
         let utterance = AVSpeechUtterance(string: text)
